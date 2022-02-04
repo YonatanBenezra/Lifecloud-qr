9794512788e9ff9301ef76aa0c677507a51bf01c
YonatanBenezra <yonatanbenezra1@gmail.com>
2022-02-04 16:09:16 +0200

    test

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -3,10 +3,8 @@ import { Redirect } from 'react-router-dom';
 
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: 'LOGIN_START' });
-  console.log(process.env.REACT_APP_API_URL)
   try {
     const res = await axios.post((`${process.env.REACT_APP_API_URL}/api/auth/login`), userCredential);
-    console.log(res, 'res')
     let username = `${res.data.firstName} ${res.data.lastName}`
     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
   } catch (err) {
