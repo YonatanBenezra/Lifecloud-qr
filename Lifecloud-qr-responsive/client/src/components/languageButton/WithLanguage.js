9a8b1114b5466cec43663e025ce7ba2197048879
R4hul Yadav <49819046+cybng@users.noreply.github.com>
2022-04-08 13:07:57 +0530

    update

--- Lifecloud-qr-responsive/client/src/components/languageButton/WithLanguage.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react'
-import LanguageContext from '../../context/LanguageContext';
-function WithLanguage({ children }) {
-    const [language, setLanguage] = useState("");
-    return <LanguageContext.Provider value={{ language, setLanguage }}>{children}</LanguageContext.Provider>;
-}
-export default WithLanguage
--- Lifecloud-qr-responsive/client/src/context/LanguageContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const LanguageContext = React.createContext({
-    language: "heb",
-    setLanguage: () => { }
-})
-
-export default LanguageContext
