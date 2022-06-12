47018c314fefc78266225c2e4c14cdc476bceb3c
niloy-koiry <niloy.koiry@node.inc>
2022-06-12 20:14:05 +0600

    Candle Flower Completed

--- server/models/CandleFlower.js
+++ server/models/CandleFlower.js
@@ -4,13 +4,18 @@ const CandleFlowerSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
-    candle:Number,
-    flower:Number
+    candle: Number,
+    flower: Number,
+    expireAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
 
   { timestamps: true }
 );
 
+CandleFlowerSchema.index({ expireAt: 1 }, { expireAfterSeconds: 500 });
 const CandleFlowerModel = mongoose.model('CandleFlower', CandleFlowerSchema);
 
 module.exports = { CandleFlowerModel };
